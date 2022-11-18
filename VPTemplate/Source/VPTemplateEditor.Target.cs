e1bc2c300ed173b1ae0f37e2e8061ab12f9677db
Cradle <>
2022-11-18 10:13:39 +0100

    Unreal 5.1 Upgrade & setup

--- VPTemplate/Source/VPTemplateEditor.Target.cs
+++ VPTemplate/Source/VPTemplateEditor.Target.cs
@@ -9,7 +9,8 @@ public class VPTemplateEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 
-		ExtraModuleNames.AddRange( new string[] { "VPTemplate" } );
+        ExtraModuleNames.AddRange( new string[] { "VPTemplate" } );
 	}
 }
