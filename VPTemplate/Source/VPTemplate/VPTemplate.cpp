795d81f6e53bcae1e264b1b54c72000899905e58
cradle@ade-buas.nl <cradle@ade-buas.nl>
2025-03-21 09:03:33 +0100

    Removed code module from template

--- VPTemplate/Source/VPTemplate/VPTemplate.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "VPTemplate.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VPTemplate, "VPTemplate" );
